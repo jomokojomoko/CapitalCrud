5fc67c896dcbbc116a2f40ebe398e07a0d4d9b75
Joseph <joseph0206chan@gmail.com>
2023-10-29 12:42:18 -0400

    Setup initial server files and package.json

---
+++ server.js
@@ -0,0 +1,25 @@
+//import statements
+const express = require("express");
+const cors = require("cors");
+
+//initialize variables
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+// add cors to server
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
