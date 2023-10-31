1f84e9caee2121f399711618d7c248ef10749e07
Joseph <joseph0206chan@gmail.com>
2023-10-30 23:38:24 -0400

    Fixed error where pagination was displayign one too many pages

--- client/src/components/paginations/employeeListPagination.js
+++ client/src/components/paginations/employeeListPagination.js
@@ -1,19 +1,19 @@
 // import packages
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Pagination from 'react-bootstrap/Pagination';
 
 // pagination component for the employee list
-function EmployeeListPagination({ numPerPage, numItems,  page, changePage}) {
+function EmployeeListPagination({ numPerPage, numItems, page, changePage}) {
     // variables
     let items = [];
-    
+
     // used to set the active page
-    function setActive(count){
+    function setActive(count) {
         changePage(count);
     }
 
     // create the paginiation items to put in the pagination
-    for (let count = 1; count <= (numItems / numPerPage) + 1 ; count++) {
+    for (let count = 1; count <= ((numItems - 1) / numPerPage) + 1 && count <= 10; count++) {
         items.push(
             <Pagination.Item onClick={() => setActive(count)} key={count} active={count === page}>
                 {count}
@@ -22,7 +22,9 @@ function EmployeeListPagination({ numPerPage, numItems,  page, changePage}) {
     }
 
     return (
-        <Pagination>{items}</Pagination>
+        <Pagination>
+            <Pagination.Prev hidden={true} /> {items}  <Pagination.Next  />
+        </Pagination>
     );
 }
 
